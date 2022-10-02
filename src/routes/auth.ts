d797d72df1c4458e17c208de7c44e8d1f86c89c0
Mike McAvoy <mcavoy.m@icloud.com>
2022-10-02 09:44:15 +0000

    added zod validation

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,4 +1,13 @@
 import express, { Request, Response } from 'express'
+import z from 'zod'
+
+const userSignUpSchema = z.object({
+    email: z.string().email(),
+    firstName: z.string().max(50),
+    surname: z.string().max(50),
+})
+
+type UserSignUpDTO = z.infer<typeof userSignUpSchema>
 
 function getAuthRoutes() {
     const router = express.Router()
@@ -9,8 +18,13 @@ function getAuthRoutes() {
 }
 
 function signUp(req: Request, res: Response) {
-    console.log(req.body)
-    res.send(true)
+    try {
+        const userSignUpDTO: UserSignUpDTO = req.body
+        userSignUpSchema.parse(userSignUpDTO)
+        res.send(true)
+    } catch (err) {
+        res.send(err)
+    }
 }
 
 export { getAuthRoutes }
