5082a211c9032c7a871da9c9a4384c55e31ed688
Mike McAvoy <mcavoy.m@icloud.com>
2022-10-06 21:28:20 +0000

    began working on sign in functionality

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,13 +1,20 @@
 import express, { Request, Response } from 'express'
 import crypto from 'crypto'
-import { CognitoIdentityProvider } from '@aws-sdk/client-cognito-identity-provider'
+import {
+    AuthFlowType,
+    CognitoIdentityProvider,
+} from '@aws-sdk/client-cognito-identity-provider'
 import z from 'zod'
 
 const REGION = ''
 const CLIENT_ID = ''
 const CLIENT_SECRET = ''
+const USER_POOL_ID = ''
 
-const cognitoIdentity = new CognitoIdentityProvider({ region: REGION })
+const cognitoIdentity = new CognitoIdentityProvider({
+    region: REGION,
+    credentials: { accessKeyId: CLIENT_ID, secretAccessKey: CLIENT_SECRET },
+})
 
 const userSignUpSchema = z.object({
     email: z.string().email(),
@@ -18,10 +25,18 @@ const userSignUpSchema = z.object({
 
 type UserSignUpDTO = z.infer<typeof userSignUpSchema>
 
+const userSignInSchema = z.object({
+    email: z.string().email(),
+    password: z.string(),
+})
+
+type UserSignInDTO = z.infer<typeof userSignInSchema>
+
 function getAuthRoutes() {
     const router = express.Router()
 
     router.post('/signup', signUp)
+    router.post('/signin', signIn)
 
     return router
 }
@@ -31,7 +46,7 @@ async function signUp(req: Request, res: Response) {
         const userSignUpDTO: UserSignUpDTO = req.body
         userSignUpSchema.parse(userSignUpDTO)
 
-        const data = await cognitoIdentity.signUp({
+        const userData = await cognitoIdentity.signUp({
             ClientId: CLIENT_ID,
             SecretHash: generateHash(userSignUpDTO.email),
             Username: userSignUpDTO.email,
@@ -41,6 +56,34 @@ async function signUp(req: Request, res: Response) {
                 { Name: 'given_name', Value: userSignUpDTO.firstName },
                 { Name: 'family_name', Value: userSignUpDTO.surname },
             ],
+        })
+
+        const confirmedUserData = await cognitoIdentity.adminConfirmSignUp({
+            UserPoolId: USER_POOL_ID,
+            Username: userSignUpDTO.email,
+        })
+
+        console.log(confirmedUserData)
+
+        res.status(200).send(true)
+    } catch (err) {
+        res.send(err)
+    }
+}
+
+async function signIn(req: Request, res: Response) {
+    try {
+        const userSignInDTO: UserSignInDTO = req.body
+        userSignInSchema.parse(userSignInDTO)
+
+        const data = await cognitoIdentity.initiateAuth({
+            AuthFlow: AuthFlowType.USER_PASSWORD_AUTH,
+            ClientId: CLIENT_ID,
+            AuthParameters: {
+                USERNAME: userSignInDTO.email,
+                PASSWORD: userSignInDTO.password,
+                SECRET_HASH: generateHash(userSignInDTO.email),
+            },
         })
 
         console.log(data)
